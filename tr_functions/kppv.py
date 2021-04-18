cfc42b31bc7aab09a0dfe6b1892f4f7a5920afb9
Thomas Renne <thomas.renne@u-psud.fr>
2021-04-18 18:09:10 +0200

    Add the cross validation to kppv

--- tr_functions/kppv.py
+++ tr_functions/kppv.py
@@ -9,6 +9,7 @@ class KppvModel(GeneralModel):
     def __init__(self):
         self.__k = 1
         self.__cross_val = 1
+        self.__k_results_cv = []
         self.__vote_method = "unanimous"
         GeneralModel.__init__(self)
     
@@ -60,9 +61,41 @@ class KppvModel(GeneralModel):
             row_num = int(line[0]) - 1
             col_num = int(k_list[0][0]) -1
             self._conf_matrix[row_num, col_num] = self._conf_matrix[row_num, col_num] + 1
-       
+    
+    def get_k_cross_validation(self,app_data, k_max, cv):
+        self.__cross_val = cv
+        best_k = [0, 0]
+        df = pd.DataFrame(app_data)
+        shuffled = df.sample(frac=1)
+        cut_dfs = np.array_split(shuffled, self.__cross_val)  
+        for i in range(k_max):
+            self.__k = i + 1
+            sum_error = 0
+            for i in range(self.__cross_val):
+                df_cv = cut_dfs[i]
+                df_train = df.drop(df_cv.index)
+                df_cv = df_cv.values.tolist()
+                df_train = df_train.values.tolist()
+                count_top_1 = 0
+                for line in df_cv:
+                    kppv_list = self.__get_kppv_list(line[1:], df_train)
+                    count_dict = Counter(kppv_list[0])
+                    temp = max(count_dict.values()) 
+                    res = [key for key in count_dict if count_dict[key] == temp] 
+                    if res[0] == line[0]:
+                        count_top_1 = count_top_1 + 1
+                error_rate = count_top_1/len(df_cv)
+                sum_error = sum_error + error_rate
+            self.__k_results_cv.append((self.__k,sum_error/self.__cross_val))
+            if best_k[1] < sum_error/self.__cross_val:
+                best_k[0] = self.__k
+                best_k[1] = sum_error/self.__cross_val
+        self.__plot_k_error_rate()
+        print("The best k found is {} with a error rate = {}".format(best_k[0], best_k[1]))
+        self.k = best_k[0]
+
     #Private methods 
-    def __get_kppv_list(self, dec_point:list):
+    def __get_kppv_list(self, dec_point:list, train_data=None):
         """Compute all distances between dec_point and each training point then keep only the k best distances.
 
         Args:
@@ -70,9 +103,11 @@ class KppvModel(GeneralModel):
 
         Returns:
             list: list of lists with the class number and the score in each
-        """        
+        """     
+        if train_data == None:
+            train_data = self._train_data   
         dist_list = []
-        for app_line in self._train_data:
+        for app_line in train_data:
             dist_list.append([app_line[0], self._compute_one_euclidian_dist(app_line[1:3], dec_point)])
         app_df = pd.DataFrame(dist_list)
         sorted_df = app_df.sort_values(by=1)
@@ -131,37 +166,10 @@ class KppvModel(GeneralModel):
                 
         else:
             self._error_count += 1
-        
-    # def get_majority_result(kppv_list):
-    #     count_dict = Counter(kppv_list[0])
-    #     temp = max(count_dict.values()) 
-    #     res = [key for key in count_dict if count_dict[key] == temp] 
-    #     return res
+     
+    def __plot_k_error_rate(self):
+        print("TODO")
+        print(self.__k_results_cv)
 
-    # def get_k_cross_validation(app_data, k_max, cv):
-    #     best_k = [0, 0]
-    #     df = pd.DataFrame(app_data)
-    #     shuffled = df.sample(frac=1)
-    #     cut_dfs = np.array_split(shuffled, cv)  
-    #     for i in range(k_max):
-    #         k = i + 1
-    #         sum_error = 0
-    #         for i in range(cv):
-    #             df_cv = cut_dfs[i]
-    #             df_train = df.drop(df_cv.index)
-    #             df_cv = df_cv.values.tolist()
-    #             df_train = df_train.values.tolist()
-    #             count_top_1 = 0
-    #             for line in df_cv:
-    #                 kppv_list = get_kppv_list(df_train, line[1:], k)
-    #                 res = get_majority_result(kppv_list)
-    #                 if res[0] == line[0]:
-    #                     count_top_1 = count_top_1 + 1
-    #             error_rate = count_top_1/len(df_cv)
-    #             sum_error = sum_error + error_rate
-    #         if best_k[1] < sum_error/cv:
-    #             best_k[0] = k
-    #             best_k[1] = sum_error/cv
-    #     return best_k
 
    
